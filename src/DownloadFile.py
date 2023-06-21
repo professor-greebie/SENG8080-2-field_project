1c46d33480e118685e889cdb1c899eddebe88117
pranshumahajan <pranshumahajan2001@gmail.com>
2023-06-21 23:18:06 +0300

    Add exception handling to handle potential errors, Rename chunk_size to CHUNK_SIZE, and exception handling, using the pathlib module for file paths, and using the logging module for logging messages instead of print statements.

--- src/DownloadFile.py
+++ src/DownloadFile.py
@@ -1,41 +1,51 @@
 import requests
-import boto3
 import os
+import logging
+from pathlib import Path
+import validators
 
-def validatePath(path):
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def validate_path(path):
     if not os.path.exists(path):
-        print("Not a valid Path")
+        logger.error("Invalid path: %s", path)
         return False
     return True
 
-def DownloadFile(fileToDownloadURL, localFileSavePath):
-    outputfileName = fileToDownloadURL.split("/")[-1]
+def validate_url(url):
+    if not validators.url(url):
+        logger.error("Invalid URL: %s", url)
+        return False
+    return True
 
-    #checking is path where file will be stored is exists or not.
-    if validatePath(localFileSavePath):
-        #Download the file from the URL
-        response = requests.get(fileToDownloadURL, stream=True)
+def download_file(file_to_download_url, local_file_save_path):
+    if not validate_url(file_to_download_url) or not validate_path(local_file_save_path):
+        return
 
-        # Save the file locally
-        if response.status_code == 200:
-            fileSave = localFileSavePath +"/"+ outputfileName
-            with open(fileSave, "wb") as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    file.write(chunk)
+    output_file_name = file_to_download_url.split("/")[-1]
+    file_save_path = Path(local_file_save_path) / output_file_name
 
-            print("File Downloaded Successfully.")
-            print("File Location : " + localFileSavePath)
-            print("File Name : " + outputfileName)
-        else:
-            print("Failed to Download The File.")
-
+    try:
+        with requests.Session() as session:
+            file_response = session.get(file_to_download_url, stream=True)
+            file_response.raise_for_status()
+            with open(file_save_path, "wb") as file_save:
+                for chunk in file_response.iter_content(chunk_size=8192):
+                    file_save.write(chunk)
+        logger.info("File downloaded successfully.")
+        logger.info("File location: %s", file_save_path)
+    except requests.RequestException as e:
+        logger.error("Failed to download the file: %s", str(e))
 
 if __name__ == "__main__":
-    streamPath = input("Enter path of file to be Download : ")
-    downloadPath = input("Enter Local Drive Path where file will be saved : ")
-    DownloadFile(streamPath.lstrip().rstrip(), downloadPath.lstrip().rstrip())
+    file_urls = [
+        "https://hgdownload.soe.ucsc.edu/goldenPath/dipOrd1/bigZips/genes/dipOrd1.ensGene.gtf.gz",
+        "https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/genes/ncbiRefSeq.gtf.gz",
+        "https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/genes/refGene.gtf.gz"
+    ]
+    local_file_save_path = input("Enter the local drive path where files will be saved: ").strip()
 
-#As per Data Research Team, below three files will be required to download.
-#https://hgdownload.soe.ucsc.edu/goldenPath/dipOrd1/bigZips/genes/dipOrd1.ensGene.gtf.gz 
-#https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/genes/ncbiRefSeq.gtf.gz 
-#https://hgdownload.soe.ucsc.edu/goldenPath/rn7/bigZips/genes/refGene.gtf.gz
+    for file_url in file_urls:
+        download_file(file_url, local_file_save_path)
